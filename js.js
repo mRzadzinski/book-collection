ea00c6d45bdcb645302e359c27239ba4b6c10256
Maciej Rzadzinski <maciej.rzd@gmail.com>
2022-10-05 18:39:30 +0200

    Structure HTML, add table and modal

---
+++ js.js
@@ -0,0 +1,12 @@
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+        return `${title}, ${author}, ${pages} pages, ${read}`
+    }
+}
+
+let lotr = new Book('Lord of the Rings', 'Tolkien', 370, 'read');
+
