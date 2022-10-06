c27910827be69a275308a595983cf555f209fbe2
Maciej Rzadzinski <maciej.rzd@gmail.com>
2022-10-06 19:25:01 +0200

    Use book collection to fill table data

--- js.js
+++ js.js
@@ -1,13 +1,65 @@
-function Book(title, author, pages, read) {
+const bookCollection = [];
+let counter = 0;
+
+function Book(title, author, pages, finished) {
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = read
+    this.finished = finished
     this.info = function() {
-        return `${title}, ${author}, ${pages} pages, ${read}`
+        return `${title}, ${author}, ${pages} pages, ${finished}`
     }
 }
 
-let lotr = new Book('Lord of the Rings', 'Tolkien', 370, 'read');
+function addBookToCollection(book) {
+    bookCollection.push(book);
+}
 
-const myModal = document.getElementById('myModal');
+function removeBookFromCollection() {
+
+}
+
+let dontMakeMeThink = new Book('Don\'t Make Me Think', 'Steve Krug', 216, 'NO');
+let holdMeTight = new Book('Hold Me Tight', 'Sue Johnson', 320, 'YES');
+let meditations = new Book('Meditations', 'Marcus Aurelius', 88, 'NO');
+let taoTeChing = new Book('Tao Te Ching', 'Lao Tzu', 107, 'YES');
+
+addBookToCollection(dontMakeMeThink);
+addBookToCollection(holdMeTight);
+addBookToCollection(meditations);
+addBookToCollection(taoTeChing);
+
+const tableBody = document.querySelector('tbody');
+const rowTemplate = document.getElementById('row-template');
+const clone = rowTemplate.cloneNode(true);
+clone.removeAttribute('class');
+clone.removeAttribute('id');
+
+
+function displayCollection(collection) {
+    collection.forEach(book => {
+        // Create new row for each book
+        let newClone = clone.cloneNode(true);
+        tableBody.appendChild(newClone);
+        let row = tableBody.lastChild.querySelectorAll('.align-middle');
+        
+        Object.values(book)[0];
+        
+        // Iterate through rows and add book data
+        for (let i = 0; i < row.length - 1; i++) {
+            if (i === 0) {
+                counter++;
+                row[i].innerHTML = counter;
+                continue;
+            } else if (i === 4) {
+                row[i].children[0].innerHTML = Object.values(book)[i - 1];
+                if (Object.values(book)[i - 1] === 'YES') {
+                    row[i].children[0].classList.remove('btn-outline-secondary');
+                    row[i].children[0].classList.add('btn-outline-success');
+                }
+                continue;
+            }
+            row[i].innerHTML = Object.values(book)[i - 1];
+        }
+    });
+}
